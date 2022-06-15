bed611af6e2968cf7897b6353fffcff40b9d0d58
Wamuyu <michellewachira25@gmail.com>
2022-06-16 00:57:02 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() + 0.1) + 1;
+
+
+const num3 = 11;
+const num4 = 7;
+const mod = num3 % num4;
+
+const max = Math.max(15, 10, 5, 20);
